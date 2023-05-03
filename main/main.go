8f867b30ef042ea2193d33e7cbd255026265ee58
hugolw <hugolw@kth.se>
2023-05-03 21:08:04 +0200

    Move the GUI functions from main to gui.go in the GUI directory

--- main/main.go
+++ main/main.go
@@ -9,15 +9,10 @@
 package main
 
 import (
+	. "project/main/GUI"
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 	"log"
 	"os"
 	. "project/main/event"
@@ -46,76 +41,8 @@ func main() {
 		terminalTemplate()
 	*/
 
-	runGUI()
+	RunGUI()
 
-}
-
-func runGUI() {
-	app := app.New()
-
-	openWindow(app)
-
-	app.Run()
-}
-
-func openWindow(app fyne.App) {
-	// start window
-	start := app.NewWindow("Start")
-	start.CenterOnScreen()
-	start.Resize(fyne.NewSize(200, 100))
-	contentsOfStartWindow(app, start)
-
-	start.Show()
-}
-
-func contentsOfStartWindow(app fyne.App, start fyne.Window) {
-	//  ok-start button
-	okButton := widget.NewButton("ok", func() {
-		mainWindow(app)
-		start.Close()
-	})
-
-	myTextStyle := fyne.TextStyle{Bold: true, Monospace: true}
-	welcomeMessage := widget.NewLabel("Welcome to Swedish Police Events, press ok to get started!")
-	welcomeMessage.TextStyle = myTextStyle
-
-	startContent := container.New(layout.NewVBoxLayout(),
-		welcomeMessage,
-		okButton)
-
-	start.SetContent(startContent)
-}
-
-func mainWindow(app fyne.App) {
-	// main window
-	windowMain := app.NewWindow("SwePe")
-	windowMain.Resize(fyne.NewSize(500, 600))
-	windowMain.CenterOnScreen()
-
-	contentsOfMainWindow := container.New(layout.NewVBoxLayout(), verticalToolbar())
-
-	windowMain.SetContent(contentsOfMainWindow)
-
-	windowMain.Show()
-
-}
-
-func verticalToolbar() *widget.Toolbar {
-	toolbar := widget.NewToolbar(
-		widget.NewToolbarAction(theme.SettingsIcon(), func() {
-			fmt.Println("Display settings")
-		}),
-		widget.NewToolbarAction(theme.SearchIcon(), func() {
-			fmt.Println("Display search")
-		}),
-		widget.NewToolbarAction(theme.DocumentSaveIcon(), func() {
-			fmt.Println("Display save")
-		}),
-		widget.NewToolbarAction(theme.HelpIcon(), func() {
-			log.Println("Display help")
-		}),
-	)
-	return toolbar
 }
 
 // Merges new and old events into a single slice
