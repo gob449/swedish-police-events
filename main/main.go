087c2f88bb1b7613cf88f7596632204857e06459
gob449 <114908033+gob449@users.noreply.github.com>
2023-05-04 15:48:37 +0200

    moved all eventrelated functions to event.go in event dir (#30) (#31)

--- main/main.go
+++ main/main.go
@@ -9,9 +9,6 @@
 package main
 
 import (
-	. "project/main/GUI"
-	"bufio"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -20,116 +17,17 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/gocolly/colly"
-	"github.com/pkg/browser"
 )
 
 // Collects the new data and merges it with the previous data in the database
 // Calls terminalTemplate() to start the program
 func main() {
-	/*
-		// Archive data
-		eventsInArchive := getArchive()
-		// New data
-		newEvents := getNewEvents()
-		// Merge old event with new events. Also, save the amount of duplicates in variable (could be useful)
-		mergedEvents, _ := mergeEvents(eventsInArchive, newEvents)
-		// Save new slice of events in archive
-		saveInArchive(mergedEvents)
-		// start program
-		terminalTemplate()
-	*/
 
-	RunGUI()
+	// Run terminal program
+	//terminalTemplate()
 
-}
+	//RunGUI()
 
-// Merges new and old events into a single slice
-func mergeEvents(eventsInArchive []Event, newEvents []Event) ([]Event, int) {
-	// Old and new with duplicates
-	allEventsRaw := append(eventsInArchive, newEvents...)
-	var mergedEvents []Event
-	visited := make(map[int]int)
-	var duplicates int
-	// Time complexity is O(n^2) because "append" creates a new slice which is O(n) for every new event
-	for _, event := range allEventsRaw {
-		_, ok := visited[event.Id]
-		if !ok {
-			visited[event.Id]++
-			mergedEvents = append(mergedEvents, event)
-		} else {
-			duplicates++
-		}
-	}
-	return mergedEvents, duplicates
-}
-
-// returns the new data of type event
-func getNewEvents() []Event {
-	newData := bytesFromAPI()
-	newEvents := eventCreator(newData)
-	return newEvents
-}
-
-// Returns the events that are currently stored in the archive
-func getArchive() []Event {
-	data, _ := os.ReadFile("main/archive/archive.json")
-	eventsInArchive := eventCreator(data)
-	return eventsInArchive
-}
-
-// From byte data to structs of type event
-func eventCreator(data []byte) []Event {
-	var events []Event
-	if err := json.Unmarshal(data, &events); err != nil {
-		fmt.Println("Error occurred while creating events")
-		log.Fatal(err)
-	}
-	return events
-}
-
-// Scrapes API-data and returns byte arr
-func bytesFromAPI() []byte {
-	var data []byte
-	c := colly.NewCollector()
-	c.OnError(func(response *colly.Response, err error) {
-		log.Fatal(err)
-	})
-	c.OnResponse(func(response *colly.Response) {
-		data = response.Body
-	})
-	if err := c.Visit("https://polisen.se/api/events"); err != nil {
-		log.Fatal(err)
-	}
-	return data
-}
-
-// saveInArchive saves a slice of events in a JSON file located at "main/archive/archive.json".
-// If the file doesn't exist, it creates the file. If the file already exists, it appends the data.
-// The function returns an error if any error occurs during file operations.
-func saveInArchive(events []Event) {
-	// Creates file if necessary, appends if file exists
-	file, err := os.OpenFile("main/archive/archive.json", os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		fmt.Println("An error occurred while trying to open the archive")
-		log.Fatal(err)
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			fmt.Println("An error occurred while trying to close the archive")
-			log.Fatal(err)
-		}
-	}()
-	writer := bufio.NewWriter(file)
-	data, err := json.Marshal(events)
-	if err != nil {
-		fmt.Println("An error occurred while trying to store data in the archive")
-	}
-	if _, err := writer.Write(data); err != nil {
-		fmt.Println("An error occurred while trying to store data in the archive")
-		log.Fatal(err)
-	}
 }
 
 // Prints the overall menu in the terminal
@@ -207,7 +105,7 @@ func printSpecificTypeInTerminal() {
 		log.Fatal(err)
 	}
 	lowerType := strings.ToLower(typeSearch)
-	eventsInArchive := getArchive()
+	eventsInArchive := GetArchive()
 
 	sort.Sort(ByType(eventsInArchive))
 
@@ -236,7 +134,7 @@ func printSpecificLocationInTerminal() {
 	}
 	lowerLocation := strings.ToLower(locationSearch)
 
-	eventsInArchive := getArchive()
+	eventsInArchive := GetArchive()
 
 	sort.Sort(ByLocation(eventsInArchive))
 
@@ -255,7 +153,7 @@ func printSpecificLocationInTerminal() {
 
 // Prints the names and Id:s of the crimes, based on its Datetime
 func printDatetimeInTerminal() {
-	eventsInArchive := getArchive()
+	eventsInArchive := GetArchive()
 	sort.Sort(ByDatetime(eventsInArchive))
 	for _, event := range eventsInArchive {
 		fmt.Println(event.Id, "----", event.Name)
@@ -264,7 +162,7 @@ func printDatetimeInTerminal() {
 
 // Prints the names and Id:s of the crimes, based on its Id
 func printIdsInTerminal() {
-	eventsInArchive := getArchive()
+	eventsInArchive := GetArchive()
 	sort.Sort(ById(eventsInArchive))
 	for _, event := range eventsInArchive {
 		fmt.Println(event.Id, "----", event.Name)
@@ -284,7 +182,7 @@ func printSpecificSummaryInTerminal() {
 		fmt.Println("An error occurred while parsing user input")
 		log.Fatal(err)
 	}
-	eventsInArchive := getArchive()
+	eventsInArchive := GetArchive()
 	for _, event := range eventsInArchive {
 		if key == event.Id {
 			fmt.Println(event.Name, "----", event.Summary)
@@ -305,18 +203,10 @@ func parseInputForID() {
 		fmt.Println("An error occurred while parsing user input")
 		log.Fatal(err)
 	}
-	eventsInArchive := getArchive()
+	eventsInArchive := GetArchive()
 	for _, event := range eventsInArchive {
 		if key == event.Id {
-			openSummary(event.Url)
+			OpenSummary(event.Url)
 		}
-	}
-}
-
-// Takes Event.URL value and opens a webpage with the corresponding extensive event summary
-func openSummary(URL string) {
-	URL = "https://polisen.se/" + URL
-	if err := browser.OpenURL(URL); err != nil {
-		fmt.Println("An error occurred while trying to open the event summary page.")
 	}
 }
